a2ba0a385cdce964f5c1774058e2b9fbcdf6620c
BogdanDomashenko <got.bogdan@gmail.com>
2022-02-07 18:07:47 +0200

    added routing

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div className="pageNotFound">
+      <h1>404</h1>
+      <p>Страница не найдена</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
