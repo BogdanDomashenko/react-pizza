a865c0e1e28546d850131a68b4105ef2882b0c55
BogdanDomashenko <got.bogdan@gmail.com>
2022-02-08 01:53:23 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import { Home, Cart, PageNotFound } from "./pages";
 import { Header } from "./components";
 
 import "./index.css";
+import axios from "axios";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("./db.json").then((response) => {
+      setPizzas(response.data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home items={pizzas} />} />
           <Route exact path="/cart" element={<Cart />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Categories } from "../components/Categories";
 export { default as Header } from "../components/Header";
 export { default as SortPopup } from "../components/SortPopup";
+export { default as PizzaBlock } from "../components/PizzaBlock";
