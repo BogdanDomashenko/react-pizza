3a57dc83849b19912b17bb9a5a577dbf082d1b32
bogdan <got.bogdan@gmail.com>
2022-07-08 19:31:16 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,90 +1,93 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, {useEffect} from "react";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import {
-	Header,
-	Modals,
-	Orders,
-	Products,
-	ProtectedRoute,
-	Statistics,
-	Stock,
-	Users,
+    Header,
+    Modals,
+    Orders,
+    Products,
+    ProtectedRoute,
+    Statistics,
+    Stock,
+    Users,
 } from "./components";
-import { useRole } from "./hooks";
+import {useRole} from "./hooks";
 import "./index.css";
 import {
-	Admin,
-	Cart,
-	Home,
-	Order,
-	PageNotFound,
-	Profile,
-	SignIn,
-	SignUp,
+    Admin,
+    Cart,
+    Home,
+    Order,
+    PageNotFound,
+    Profile,
+    SignIn,
+    SignUp,
 } from "./pages";
-import { ROLES } from "./utils/constants";
+import {ROLES} from "./utils/constants";
+import {getAccessToken} from "./services/localStorage.service";
+import {useDispatch} from "react-redux";
+import {logout} from "./redux/actions/user";
 
 function App() {
-	const isAuth = useRole() !== ROLES.phantom;
+    const isAuth = useRole() !== ROLES.phantom;
 
-	return (
-		<div className="wrapper">
-			<Modals />
-			<Header />
-			<div className="content">
-				<Routes>
-					<Route exact path="/" element={<Home />} />
-					<Route exact path="/cart" element={<Cart />} />
-					<Route exact path="/order/:id" element={<Order />} />
-					<Route
-						exact
-						path="/admin"
-						element={
-							<ProtectedRoute allowedRoles={[ROLES.admin]}>
-								<Admin />
-							</ProtectedRoute>
-						}
-					>
-						<Route exact path="products" element={<Products />} />
-						<Route exact path="orders" element={<Orders />} />
-						<Route exact path="stock" element={<Stock />} />
-						<Route exact path="statistics" element={<Statistics />} />
-						<Route exact path="users" element={<Users />} />
-					</Route>
-					<Route exact path="/auth">
-						<Route
-							exact
-							path="sign-in"
-							element={
-								<ProtectedRoute mustBeAuth={false} isAuth={isAuth}>
-									<SignIn />
-								</ProtectedRoute>
-							}
-						/>
-						<Route
-							exact
-							path="sign-up"
-							element={
-								<ProtectedRoute mustBeAuth={false} isAuth={isAuth}>
-									<SignUp />
-								</ProtectedRoute>
-							}
-						/>
-					</Route>
-					<Route
-						exatc
-						path="/profile"
-						element={
-							<ProtectedRoute mustBeAuth={true} isAuth={isAuth}>
-								<Profile />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</div>
-		</div>
-	);
+    return (
+        <div className="wrapper">
+            <Modals/>
+            <Header/>
+            <div className="content">
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/cart" element={<Cart/>}/>
+                    <Route exact path="/order/:id" element={<Order/>}/>
+                    <Route
+                        exact
+                        path="/admin"
+                        element={
+                            <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                                <Admin/>
+                            </ProtectedRoute>
+                        }
+                    >
+                        <Route exact path="products" element={<Products/>}/>
+                        <Route exact path="orders" element={<Orders/>}/>
+                        <Route exact path="stock" element={<Stock/>}/>
+                        <Route exact path="statistics" element={<Statistics/>}/>
+                        <Route exact path="users" element={<Users/>}/>
+                    </Route>
+                    <Route exact path="/auth">
+                        <Route
+                            exact
+                            path="sign-in"
+                            element={
+                                <ProtectedRoute mustBeAuth={false} isAuth={isAuth}>
+                                    <SignIn/>
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            exact
+                            path="sign-up"
+                            element={
+                                <ProtectedRoute mustBeAuth={false} isAuth={isAuth}>
+                                    <SignUp/>
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Route>
+                    <Route
+                        exatc
+                        path="/profile"
+                        element={
+                            <ProtectedRoute mustBeAuth={true} isAuth={isAuth}>
+                                <Profile/>
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="*" element={<PageNotFound/>}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 // const mapStateToProps = (state) => {
