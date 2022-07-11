700932b9acd1d568b15745d2f38ae548c477c671
bogdan <got.bogdan@gmail.com>
2022-07-11 19:23:13 +0300

    order fix

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,43 +1,37 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { MODALS, ROLES } from "../utils/constants";
-import { CartItem, ClearCart } from "../components";
-import { Button, CartIcon, GoBackIcon } from "../components/ui";
-import {
-	cartItemCountDec,
-	cartItemCountInc,
-	checkoutCart,
-	removeCartItem,
-	resetCart,
-} from "../redux/actions/cart";
-import { toggleModalVisibility } from "../redux/actions/modals";
-import { useRole, useUserData } from "../hooks";
+import {useDispatch, useSelector} from "react-redux";
+import {Link} from "react-router-dom";
+import {MODALS, ROLES} from "../utils/constants";
+import {CartItem, ClearCart} from "../components";
+import {Button, CartIcon, GoBackIcon} from "../components/ui";
+import {cartItemCountDec, cartItemCountInc, checkoutCart, removeCartItem, resetCart,} from "../redux/actions/cart";
+import {toggleModalVisibility} from "../redux/actions/modals";
+import {useRole, useUserData} from "../hooks";
 import {Oval} from "react-loader-spinner";
 
 const Cart = () => {
 	const dispatch = useDispatch();
 	const role = useRole();
 	const user = useUserData();
-	const { items, totalPrice, totalCount, isCheckouting } = useSelector((state) => state.cart);
+	const {items, totalPrice, totalCount, isCheckouting} = useSelector((state) => state.cart);
 	const itemsValues = Object.values(items);
-
+	
 	const onClearCartClick = () => {
 		dispatch(resetCart());
 	};
-
+	
 	const onRemoveCartItem = (id) => {
 		dispatch(removeCartItem(id));
 	};
-
+	
 	const onIncCartItemCount = (id) => {
 		dispatch(cartItemCountInc(id));
 	};
-
+	
 	const onDecCartItemCount = (id) => {
 		dispatch(cartItemCountDec(id));
 	};
-
+	
 	const onBuyButtonClick = () => {
 		if (role === ROLES.phantom) {
 			dispatch(toggleModalVisibility(MODALS.СheckoutModal));
@@ -45,11 +39,11 @@ const Cart = () => {
 			dispatch(checkoutCart(user.phoneNumber));
 		}
 	};
-
+	
 	return (
 		<div className="wrapper cart-wrapper">
 			<div className="container container--medium">
-				{ isCheckouting ? <div className="loader-wrapper">
+				{isCheckouting ? <div className="loader-wrapper">
 						<Oval
 							ariaLabel="loading-indicator"
 							height={100}
@@ -61,72 +55,72 @@ const Cart = () => {
 						/>
 					</div> :
 					<div className="cart">
-					<div className="cart__top">
-						<h2 className="content__title">
-							<CartIcon />
-							Cart
-						</h2>
-						{itemsValues.length ? <ClearCart onClick={onClearCartClick} /> : ""}
-					</div>
-					<div>
-						<div className="content__items">
-							{itemsValues.length
-								? itemsValues.map(
-									({ item, selectedProps, count, totalPrice }) => {
-										const id = `${item.id}_${selectedProps.type}_${selectedProps.size}`;
-										return (
-											<CartItem
-												title={item.name}
-												price={totalPrice}
-												count={count}
-												img={item.imageUrl}
-												onRemove={() => onRemoveCartItem(id)}
-												onIncCount={() => onIncCartItemCount(id)}
-												onDecCount={() => onDecCartItemCount(id)}
-												key={id}
-												selectedProps={selectedProps}
-											/>
-										);
-									}
-								)
-								: ""}
+						<div className="cart__top">
+							<h2 className="content__title">
+								<CartIcon/>
+								Cart
+							</h2>
+							{itemsValues.length ? <ClearCart onClick={onClearCartClick}/> : ""}
 						</div>
-						<div className="cart__bottom">
-							{itemsValues.length ? (
-								<div>
-									{" "}
-									<div className="cart__bottom-details">
+						<div>
+							<div className="content__items">
+								{itemsValues.length
+									? itemsValues.map(
+										({item, selectedProps, count, totalPrice}) => {
+											const id = `${item.id}_${selectedProps.type}_${selectedProps.size}`;
+											return (
+												<CartItem
+													title={item.name}
+													price={totalPrice}
+													count={count}
+													img={item.imageUrl}
+													onRemove={() => onRemoveCartItem(id)}
+													onIncCount={() => onIncCartItemCount(id)}
+													onDecCount={() => onDecCartItemCount(id)}
+													key={id}
+													selectedProps={selectedProps}
+												/>
+											);
+										}
+									)
+									: ""}
+							</div>
+							<div className="cart__bottom">
+								{itemsValues.length ? (
+									<div>
+										{" "}
+										<div className="cart__bottom-details">
 									<span>
 										{" "}
 										Total count: <b>{totalCount} pieces</b>{" "}
 									</span>
-										<span>
+											<span>
 										{" "}
-											Total price: <b>{totalPrice} $</b>{" "}
+												Total price: <b>{totalPrice} $</b>{" "}
 									</span>
-									</div>
-									<div className="cart__bottom-buttons">
-										<Link to="/">
-											{" "}
-											<Button
-												href="/"
-												className="button--outline button--add go-back-btn"
-											>
-												<GoBackIcon />
-												<span>Go back</span>
+										</div>
+										<div className="cart__bottom-buttons">
+											<Link to="/" className="cart__bottom__button">
+												{" "}
+												<Button
+													href="/"
+													className="button--outline button--add go-back-btn"
+												>
+													<GoBackIcon/>
+													<span>Go back</span>
+												</Button>
+											</Link>
+											<Button className="pay-btn cart__bottom__button" onClick={onBuyButtonClick}>
+												<span>Checkout</span>
 											</Button>
-										</Link>
-										<Button className="pay-btn" onClick={onBuyButtonClick}>
-											<span>Checkout</span>
-										</Button>
+										</div>
 									</div>
-								</div>
-							) : (
-								<h3>Cart is empty</h3>
-							)}
+								) : (
+									<h3>Cart is empty</h3>
+								)}
+							</div>
 						</div>
-					</div>
-				</div>}
+					</div>}
 			</div>
 		</div>
 	);
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,62 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { OrderItem } from "../components";
-import { setOrderId } from "../redux/actions/cart";
-import { fetchOrder } from "../services/order.service";
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useParams} from "react-router-dom";
+import {OrderItem} from "../components";
+import {setOrderId} from "../redux/actions/cart";
+import {fetchOrder} from "../services/order.service";
 
 const Order = () => {
-  const dispatch = useDispatch();
-
-  const { id } = useParams();
-  const { orderId } = useSelector((state) => state.cart);
-  const [order, setOrder] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (orderId) {
-      dispatch(setOrderId(null));
-    }
-  }, [orderId]);
-
-  useEffect(async () => {
-    try {
-      setOrder(await fetchOrder(id));
-    } catch (error) {
-      setError(error.response.data.message);
-    }
-  }, []);
-
-  return (
-    <div className="order wrapper">
-      <div className="container container--medium">
-        {!error ? (
-          order ? (
-            <div>
-              <div className="order__header">
-                <h1>Order #{id}</h1>
-                <h3>{order.status}</h3>
-              </div>
-              {order.pizzas.map((pizza, index) => (
-                <OrderItem
-                  key={index}
-                  title={pizza.name}
-                  count={pizza.pizzaOrders.count}
-                  price={pizza.pizzaOrders.totalPrice}
-                  img={pizza.imageUrl}
-                  props={pizza.pizzaOrders.props}
-                />
-              ))}
-            </div>
-          ) : (
-            ""
-          )
-        ) : (
-          <h2>{error}</h2>
-        )}
-      </div>
-    </div>
-  );
+	const dispatch = useDispatch();
+	
+	const {id} = useParams();
+	const {orderId} = useSelector((state) => state.cart);
+	const [order, setOrder] = useState(null);
+	const [error, setError] = useState(null);
+	
+	useEffect(() => {
+		if (orderId) {
+			dispatch(setOrderId(null));
+		}
+	}, [orderId]);
+	
+	useEffect(async () => {
+		try {
+			setOrder(await fetchOrder(id));
+		} catch (error) {
+			setError(error.response.data.message);
+		}
+	}, []);
+	
+	return (
+		<div className="order wrapper">
+			<div className="container container--medium">
+				{!error ? (
+					order ? (
+						<div>
+							<div className="order__header">
+								<h1>Order #{order.id}</h1>
+								<h3>{order.status}</h3>
+							</div>
+							{order.pizzaOrders.map((item, index) => (
+								<OrderItem
+									key={index}
+									title={item.pizza.name}
+									count={item.count}
+									price={item.totalPrice}
+									img={item.pizza.imageUrl}
+									props={item.props}
+								/>
+							))}
+						</div>
+					) : (
+						""
+					)
+				) : (
+					<h2>{error}</h2>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Order;
